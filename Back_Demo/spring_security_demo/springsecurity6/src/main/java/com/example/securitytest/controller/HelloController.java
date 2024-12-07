59ba01b18be99fa57c1d0109f87f87710dc9dbdf
erenjaeger <ErenJaegerKing@qq.com>
2024-12-07 10:50:09 +0800

    springsecurity prems

--- Back_Demo/spring_security_demo/springsecurity6/src/main/java/com/example/securitytest/controller/HelloController.java
+++ Back_Demo/spring_security_demo/springsecurity6/src/main/java/com/example/securitytest/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.securitytest.controller;
 
 import com.example.securitytest.domain.ResponseResult;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("hello")
-    @PreAuthorize("hasAuthority('user')")
+    @PreAuthorize("hasAuthority('delete') AND hasAuthority('view')")
     public String hello(){
+        System.out.println(SecurityContextHolder.getContext().getAuthentication());
         return "hello world";
     }
 
